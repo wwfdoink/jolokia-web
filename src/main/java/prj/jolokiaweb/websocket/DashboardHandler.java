bd0a15495f59df18229e3cd4d655cc588b90f182
Doink <wwfdoink@gmail.com>
2017-08-15 02:19:30 +0200

    ws cleanup

--- src/main/java/prj/jolokiaweb/websocket/DashboardHandler.java
+++ src/main/java/prj/jolokiaweb/websocket/DashboardHandler.java
@@ -2,6 +2,7 @@ package prj.jolokiaweb.websocket;
 
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -36,15 +37,22 @@ public class DashboardHandler extends TextWebSocketHandler {
     }
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) {
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        if ("CLOSE".equalsIgnoreCase(message.getPayload())) {
+            session.close();
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
         addSession(session);
     }
 
     @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        if ("CLOSE".equalsIgnoreCase(message.getPayload())) {
-            session.close();
-            removeSession(session);
-        }
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+        removeSession(session);
     }
+
 }
